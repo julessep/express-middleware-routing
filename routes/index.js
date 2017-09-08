48a748949be44a88a96e1fba992be5fdf94a03e7
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-09-08 15:23:30 -0500

    added match egg

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,4 @@ router.get('/', function(req, res) {
   next()
 });
 
-// router.get('/see-our-chickens', function(req, res, next) {
-//   console.log(path.join(__dirname, `../public`, `/chickens.html`))
-//   res.sendFile(path.join(__dirname, `../public`, `/chickens.html`))
-  
-// });
-
-// router.get('/see-our-eggs', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, `../public`, `/eggs.html`))
-// });
-
-
 module.exports = router;
